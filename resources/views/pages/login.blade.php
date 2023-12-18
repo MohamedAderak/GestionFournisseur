ffc95afc88bcfc7359f1dd4978ef9212350e088c
Mohamed Aderak <91732940+MohamedAderak@users.noreply.github.com>
2023-12-18 15:44:14 +0100

    Update login.blade.php

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -64,14 +64,14 @@
               {{-- value="{{ old('email') }}" --}}
               <label class="block text-sm">
                 <span class="text-gray-700 dark:text-gray-400">Email</span>
-                <input type="email" name="email" value="medaderak5@gmail.com" required autofocus
+                <input type="email" name="email" value="mail@gmail.com" required autofocus
                   class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
                   placeholder="Example@email.com"
                 />
               </label>
               <label class="block mt-4 text-sm">
                 <span class="text-gray-700 dark:text-gray-400">Password</span>
-                <input name="password" required value="aderak2000"
+                <input name="password" required value=""
                   class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
                   placeholder="***************"
                   type="password"
